e337d39cca6d88a6a610dc7c51a4cfe07c114d15
zondklim <goger1263@gmail.com>
2025-12-14 15:56:44 +0300

    added GridDistortionAugmentation

--- src/preprocessing/transforms/__init__.py
+++ src/preprocessing/transforms/__init__.py
@@ -2,6 +2,7 @@ from .scale import ScaleAugmentation
 from .shear import ShearAugmentation
 from .erosion import ErosionAugmentation
 from .dilation import DilationAugmentation
+from griddistortion import GridDistortionAugmentation
 
 __all__ = ['ScaleAugmentation',  'ShearAugmentation', 'ErosionAugmentation',
-           'DilationAugmentation']
+           'DilationAugmentation', 'GridDistortionAugmentation']
